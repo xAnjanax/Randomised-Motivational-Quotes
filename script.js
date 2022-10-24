2302119745bc2aed337bb490c67ad2f155a80b64
xAnjanax <104242574+xAnjanax@users.noreply.github.com>
2022-10-24 21:04:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function generate()
+{
+    var quotes = 
+    {
+        // "-My Fridge" : '"Close the damn door!"', 
+        // "-Your Mom" : '"Close the damn door!"', 
+        // "-Sasuke" : '"Stop being so noisy!"' 
+        "-J.R.R. Tolkein" : '"All we have to decide is what to do with the time that is given us."',
+        "-Dr Seuss" : '"You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose. You’re on your own. And you know what you know. And YOU are the one who’ll decide where to go…"', 
+        "-J.K. Rowling" : '"It matters not what someone is born, but what they grow to be."', 
+        "-Oscar Wilde" : '“Be yourself; everyone else is already taken.”', 
+        "-Martin Luther King Jr." : '"“Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.”"'
+    } // Dictionary // 
+
+    var authors = Object.keys(quotes); 
+
+    // console.log(authors); // "won't print for some reason" // 
+
+    var author = authors[Math.floor(Math.random() * authors.length)]; 
+
+    var quote = quotes[author]; 
+
+    document.getElementById("quote").innerHTML = quote; 
+    document.getElementById("author").innerHTML = author; 
+}
